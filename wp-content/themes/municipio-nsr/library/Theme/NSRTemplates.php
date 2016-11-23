3a9106f42acdbe44c5cb940ad94796193f9ff34d
Johan Silvergrund <johan.silvergrund@hiq.se>
2016-11-23 20:35:44 +0100

    template

--- wp-content/themes/municipio-nsr/library/Theme/NSRTemplates.php
+++ wp-content/themes/municipio-nsr/library/Theme/NSRTemplates.php
@@ -6,6 +6,5 @@ class NSRTemplates
 {
     public function __construct()
     {
-        //\Municipio\Helper\Template::add(__('Visual Composer Template', 'municipio'), \Municipio\Helper\Template::locateTemplate('vcomposer-page.blade.php'));
     }
 }
--- wp-content/themes/municipio-nsr/views/example-view.blade.php
+++ wp-content/themes/municipio-nsr/views/example-view.blade.php
@@ -1,4 +1,4 @@
-@extends('templates.master')
+@extends('Templates')
 
 @section('content')
     <!-- Content start -->
