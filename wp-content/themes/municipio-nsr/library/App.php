ca35f61410704ba9eeb5bfb8d861da603e9ea535
Johan Silvergrund <johan.silvergrund@hiq.se>
2016-12-05 13:26:45 +0100

    Nyheter inlägg.

--- wp-content/themes/municipio-nsr/library/App.php
+++ wp-content/themes/municipio-nsr/library/App.php
@@ -12,6 +12,9 @@ class App
 
         add_action( 'after_setup_theme', array( $this, 'nsr_theme_setup' ) );
         add_action( 'init', array( $this,'add_excerpts_to_pages' ) );
+
+        add_action( 'admin_menu', array( $this,'nsr_change_post_label' ) );
+        add_action( 'init', array( $this,'nsr_change_post_object' ) );
     }
 
 
@@ -29,9 +32,43 @@ class App
      *  add_excerpts_to_pages
      *  Adding Excerpts to page
      */
-    public function add_excerpts_to_pages() {
+    public function add_excerpts_to_pages()
+    {
         add_post_type_support( 'page', 'excerpt' );
     }
+
+
+    function nsr_change_post_label()
+    {
+
+        global $menu;
+        global $submenu;
+
+        $menu[5][0] = __('News', 'nsr');
+        $submenu['edit.php'][5][0] = __('News', 'nsr');
+        $submenu['edit.php'][10][0] = __('Add News', 'nsr');
+        $submenu['edit.php'][16][0] = __('News Tags', 'nsr');
+    }
+    function nsr_change_post_object()
+    {
+        global $wp_post_types;
+        $labels = &$wp_post_types['post']->labels;
+        $labels->name = __('News', 'nsr');
+        $labels->singular_name = __('News', 'nsr');
+        $labels->add_new = __('Add News', 'nsr');
+        $labels->add_new_item = __('Add News', 'nsr');
+        $labels->edit_item = __('Edit News', 'nsr');
+        $labels->new_item = __('Add', 'nsr');
+        $labels->view_item = __('View News', 'nsr');
+        $labels->search_items = __('Search News', 'nsr');
+        $labels->not_found = __('No news found', 'nsr');
+        $labels->not_found_in_trash = __('No news found in the trash', 'nsr');
+        $labels->all_items = __('All News', 'nsr');
+        $labels->menu_name = __('News', 'nsr');
+        $labels->name_admin_bar = __('News', 'nsr');
+    }
+
+
 
 
 
---
+++ wp-content/themes/municipio-nsr/views/partials/blog/type/post-single.blade.php
@@ -0,0 +1,28 @@
+<?php global $post; ?>
+<div class="grid">
+    <div class="grid-xs-12">
+        <div class="post post-single">
+
+            @include('partials.blog.post-header')
+
+            <article id="article">
+                @if (isset(get_extended($post->post_content)['main']) && !empty(get_extended($post->post_content)['main']) && isset(get_extended($post->post_content)['extended']) && !empty(get_extended($post->post_content)['extended']))
+
+                    {!! apply_filters('the_lead', get_extended($post->post_content)['main']) !!}
+                    {!! apply_filters('the_content', get_extended($post->post_content)['extended']) !!}
+
+                @else
+                    {!! apply_filters('the_content', $post->post_content) !!}
+                @endif
+            </article>
+
+            @if (is_single() && is_active_sidebar('content-area'))
+                <div class="grid sidebar-content-area sidebar-content-area-bottom">
+                    <?php dynamic_sidebar('content-area'); ?>
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
+
+@include('partials.blog.post-footer')
