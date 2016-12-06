f87cd0bf61924886774e815ba55ebd53d6cae34c
Johan Silvergrund <johan.silvergrund@hiq.se>
2016-12-06 13:24:00 +0100

    no changes

--- wp-content/themes/municipio/views/front-page.blade.php
+++ wp-content/themes/municipio/views/front-page.blade.php
@@ -2,44 +2,14 @@
 
 @section('content')
 
-    <div class="container main-container">
-
-        @include('partials.breadcrumbs')
-
-        <div class="grid {{ implode(' ', apply_filters('Municipio/Page/MainGrid/Classes', wp_get_post_parent_id(get_the_id()) != 0 ? array('no-margin-top') : array())) }}">
-            @include('partials.sidebar-left')
-
-            <div class="grid-md-12 grid-lg-12 grid-print-12" id="readspeaker-read">
-
-                @if (is_active_sidebar('content-area-top'))
-                    <div class="grid sidebar-content-area sidebar-content-area-top">
-                        <?php dynamic_sidebar('content-area-top'); ?>
-                    </div>
-                @endif
-
-                @while(have_posts())
-                    {!! the_post() !!}
-
-                    @include('partials.article')
-                @endwhile
-
-                @if (is_active_sidebar('content-area'))
-                    <div class="grid sidebar-content-area sidebar-content-area-bottom">
-                        <?php dynamic_sidebar('content-area'); ?>
-                    </div>
-                @endif
-
-            </div>
-
-
-
-        </div>
-
-        <div class="grid hidden-lg hidden-xl">
-            <div class="grid-sm-12">
-                @include('partials.page-footer')
-            </div>
+@if (is_active_sidebar('content-area'))
+<section class="creamy creamy-border-bottom gutter-xl gutter-vertical sidebar-content-area">
+    <div class="container">
+        <div class="grid">
+            <?php dynamic_sidebar('content-area'); ?>
         </div>
     </div>
+</section>
+@endif
 
 @stop
