3dc00886350db6f17b0ca561f79e3778f9e8cebf
Johan Silvergrund <johan.silvergrund@hiq.se>
2017-01-04 13:30:54 +0100

    new custom post type

--- wp-content/themes/municipio-nsr/library/Theme/CustomPostTypes.php
+++ wp-content/themes/municipio-nsr/library/Theme/CustomPostTypes.php
@@ -243,6 +243,63 @@ class CustomPostTypes
         register_post_type( "faq", $args );
 
 
+
+
+        /* Sorteringsguiden */
+
+        $labels = array(
+
+            "name" => __( 'Sorting guide', 'nsr' ),
+            "singular_name" => __( 'Sorting guide', 'nsr' ),
+            "menu_name" => __( 'Sorting guide', 'nsr' ),
+            "all_items" => __( 'All waste', 'nsr' ),
+            "add_new" => __( 'Add New', 'nsr' ),
+            "add_new_item" => __( 'Add New Waste', 'nsr' ),
+            "edit_item" => __( 'Edit Waste', 'nsr' ),
+            "new_item" => __( 'New Waste', 'nsr' ),
+            "view_item" => __( 'View Waste', 'nsr' ),
+            "search_items" => __( 'Search Waste', 'nsr' ),
+            "not_found" => __( 'Not Found', 'nsr' ),
+            "not_found_in_trash" => __( 'Not Found in Trash', 'nsr' ),
+            "parent_item_colon" => __( 'Parent', 'nsr' ),
+            "featured_image" => __( 'Featured Image', 'nsr' ),
+            "set_featured_image" => __( 'Set Featured Image', 'nsr' ),
+            "remove_featured_image" => __( 'Remove Featured Image', 'nsr' ),
+            "use_featured_image" => __( 'Use Featured Image', 'nsr' ),
+            "archives" => __( 'Use Featured Image', 'nsr' ),
+            "insert_into_item" => __( 'Insert into item', 'nsr' ),
+            "uploaded_to_this_item" => __( 'Uploaded to this Item', 'nsr' ),
+            "filter_items_list" => __( 'Filter Items List', 'nsr' ),
+            "items_list_navigation" => __( 'Items List Navigation', 'nsr' ),
+            "items_list" => __( 'Waste List', 'nsr' ),
+            "parent_item_colon" => __( 'Parent', 'nsr' ),
+        );
+
+        $args = array(
+            "label" => __( 'Sorting guide', 'nsr' ),
+            "labels" => $labels,
+            "description" => "Managing waste for Sorting guide",
+            "public" => true,
+            "publicly_queryable" => true,
+            "show_ui" => true,
+            "show_in_rest" => false,
+            "rest_base" => "",
+            "has_archive" => false,
+            "show_in_menu" => true,
+            "exclude_from_search" => false,
+            "capability_type" => "post",
+            "map_meta_cap" => true,
+            "hierarchical" => true,
+            "rewrite" => array( "slug" => "sorteringsguide", "with_front" => true ),
+            "query_var" => true,
+            "menu_position" => 21,
+            "menu_icon" => "dashicons-admin-page",
+            "supports" => array( "title", "editor", "excerpt", "page-attributes" ),
+            "taxonomies" => ''
+        );
+        register_post_type( "sorteringsguide", $args );
+
+
     }
 
 
