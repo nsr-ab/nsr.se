12b090f560c4f7ed18d62731b534596074a5eda9
Johan Silvergrund <johan.silvergrund@hiq.se>
2017-01-20 15:09:27 +0100

    pos

--- wp-content/plugins/nsr-vc-extended/dist/js/nsr-vc-extended.dev.js
+++ wp-content/plugins/nsr-vc-extended/dist/js/nsr-vc-extended.dev.js
@@ -364,7 +364,7 @@ VcExtended.NSRExtend.Extended = (function ($) {
         var $content = $('<ul class="search-autocomplete-content"></ul>');
         var $sorteringsguiden = $('<div class="sorteringsguiden"><h4>Sorteringsguiden</h4><div class="left badgeInfo"><span class="badge">P</span> Privat <span class="badge">F</span> Företag<br /></div></div>');
         var spinner = '<div class="preloader-wrapper small active" style="display:none;"> <div class="spinner-layer spinner-white-only"> <div class="circle-clipper left"> <div class="circle"></div> </div><div class="gap-patch"> <div class="circle"></div> </div><div class="circle-clipper right"> <div class="circle"></div> </div> </div> </div> ';
-        var $sortMarkupTable = $('<table class="sorterings-guide-table"><tr class="tabDesk"><th></th><th>Sorteras som</th><th class="relative">Lämna nära dig... '+spinner+'</th><th class="exnfodispl">Bra att veta</th></tr></table>');
+        var $sortMarkupTable = $('<table class="sorterings-guide-table"><tr class="tabDesk"><th></th><th>Sorteras som</th><th class="relative">Lämna nära dig...</th><th class="exnfodispl">Bra att veta</th></tr></table>');
 
         if (typeof res.sortguide != 'undefined' && res.sortguide !== null && res.sortguide.length > 0) {
 
@@ -445,7 +445,7 @@ VcExtended.NSRExtend.Extended = (function ($) {
                 sortHTML += '<td class="exnfodispl">'+braAttVeta+'</td>';
                 sortHTML += '</tr>';
                 sortHTML += '<tr class="tabMobile"><th>Sorteras:</th><td><ul class="meta-fraktion">'+tabMobile_frak+'</ul></td></tr>';
-                sortHTML += '<tr class="tabMobile"><th>Lämnas:</th><td><ul>'+tabMobile_inl+'</ul></td></tr>';
+                sortHTML += '<tr class="tabMobile"><th>Lämnas:</th><td>'+spinner+'<ul>'+tabMobile_inl+'</ul></td></tr>';
                 sortHTML += '<tr class="tabMobile lastchild"><td class="lastchild" colspan="2"> </td></tr>';
                 tabMobile_inl = "";
             });
