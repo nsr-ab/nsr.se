e85a8fcaadcf12a37fd5ea5f95455ded5687c451
Johan Silvergrund <johan.silvergrund@hiq.se>
2017-01-23 15:52:15 +0100

    bypassing parent !important

--- wp-content/themes/municipio-nsr/assets/dist/js/app.js
+++ wp-content/themes/municipio-nsr/assets/dist/js/app.js
@@ -1,6 +1,43 @@
 var Nsr;
 
+Nsr.App = Nsr.App || {};
 
+Nsr.App.AppDefault = (function ($) {
+
+
+    /**
+     * Constructor
+     */
+    function AppDefault() {
+        this.init();
+
+    };
+
+
+
+    /**
+     *  init
+     *  Initializes all the necessary methods
+     */
+    AppDefault.prototype.init = function () {
+        this.hackz();
+
+
+    };
+
+
+    /**
+     *  Bypase !important on parent css
+     *
+     */
+    AppDefault.prototype.hackz = function () {
+        $('.main-footer .footer-title').css('color','#007586');
+    };
+
+    return new AppDefault();
+
+    
+})(jQuery);
 
 /*!
  * Materialize v0.97.8 (http://materializecss.com)
--- wp-content/themes/municipio-nsr/assets/source/js/app.js
+++ wp-content/themes/municipio-nsr/assets/source/js/app.js
@@ -1,3 +1,40 @@
 var Nsr;
 
+Nsr.App = Nsr.App || {};
 
+Nsr.App.AppDefault = (function ($) {
+
+
+    /**
+     * Constructor
+     */
+    function AppDefault() {
+        this.init();
+
+    };
+
+
+
+    /**
+     *  init
+     *  Initializes all the necessary methods
+     */
+    AppDefault.prototype.init = function () {
+        this.hackz();
+
+
+    };
+
+
+    /**
+     *  Bypase !important on parent css
+     *
+     */
+    AppDefault.prototype.hackz = function () {
+        $('.main-footer .footer-title').css('color','#007586');
+    };
+
+    return new AppDefault();
+
+    
+})(jQuery);
