78b172187b5dab3fac7def1005f6ea2fd024af3b
Johan Silvergrund <johan.silvergrund@hiq.se>
2017-01-24 14:24:50 +0100

    Removed post excerpt

--- wp-content/themes/municipio-nsr/views/partials/article.blade.php
+++ wp-content/themes/municipio-nsr/views/partials/article.blade.php
@@ -1,10 +1,6 @@
 <?php global $post; ?>
 <article class="clearfix">
-    @if ( has_excerpt( $post->ID ) )
 
-        <h5 class="page-excertpt">{!! get_the_excerpt() !!}</h5>
-
-    @endif
 
     @if (isset(get_extended($post->post_content)['main']) && strlen(get_extended($post->post_content)['main']) > 0 && isset(get_extended($post->post_content)['extended']) && strlen(get_extended($post->post_content)['extended']) > 0)
 
