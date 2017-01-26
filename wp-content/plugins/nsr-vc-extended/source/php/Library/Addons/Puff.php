cbebb9bb957cce88e4dd727645019101069fb655
Johan Silvergrund <johan.silvergrund@hiq.se>
2017-01-26 11:14:48 +0100

    3px on "puffar" :-)

--- wp-content/plugins/nsr-vc-extended/source/php/Library/Addons/Puff.php
+++ wp-content/plugins/nsr-vc-extended/source/php/Library/Addons/Puff.php
@@ -161,7 +161,7 @@ class Puff
         if(!isset($params->vc_icon_colors))
             $params->vc_icon_colors = "#7e7f80";
 
-        $vc_border_colors = isset($params->vc_border_colors)  ? " style=\"border-top:2px solid ". $params->vc_border_colors .";\" " : null;
+        $vc_border_colors = isset($params->vc_border_colors)  ? " style=\"border-top:3px solid ". $params->vc_border_colors .";\" " : null;
         $vc_icon_colors = isset($params->vc_icon_colors)  ? " style=\"color:".$params->vc_icon_colors .";\" " : null;
         $output = "<div id=\"vc_id_".md5(date('YmdHis').rand(0,9999999))."\" ". $vc_border_colors ." class=\"card hoverable small\" >";
         $output .= "<div class=\"card-content\">";
