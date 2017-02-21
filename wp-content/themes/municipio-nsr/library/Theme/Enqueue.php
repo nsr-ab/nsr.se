36285cd8759fefff4bf09e1c7d3a6f601b0b3f89
Johan Silvergrund <johan.silvergrund@hiq.se>
2017-02-21 23:45:00 +0100

    waypoints conflict

--- wp-content/themes/municipio-nsr/library/Theme/Enqueue.php
+++ wp-content/themes/municipio-nsr/library/Theme/Enqueue.php
@@ -46,7 +46,7 @@ class Enqueue
 
         if (!apply_filters('Municipio/load-wp-jquery', false)) {
             wp_deregister_script('jquery');
-            wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
+            wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array('jquery'), null, true);
         }
     }
 
