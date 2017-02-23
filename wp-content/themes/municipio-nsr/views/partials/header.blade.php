7623f829362d07f3128933e645cebe6c3ca38ea6
Johan Silvergrund <johan.silvergrund@hiq.se>
2017-02-23 11:56:21 +0100

    print only test - i.e.

--- wp-content/themes/municipio-nsr/views/partials/header.blade.php
+++ wp-content/themes/municipio-nsr/views/partials/header.blade.php
@@ -1,16 +1,13 @@
 <header id="site-header" class="site-header">
-
-
-    @include('partials.header.' . $headerLayout['template'])
-
-    <div class="print-only container">
+    <!-- <div class="print-only container">
         <div class="grid">
             <div class="grid-sm-12">
                 {!! municipio_get_logotype('standard') !!}
             </div>
         </div>
-    </div>
+    </div> -->
 
+    @include('partials.header.' . $headerLayout['template'])
 </header>
 
 @include('partials.hero')
