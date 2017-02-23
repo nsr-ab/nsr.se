6bb2dafd7acbe05891a4f3db3e2bc99937b8f9c4
Johan Silvergrund <johan.silvergrund@hiq.se>
2017-02-23 12:03:17 +0100

    roll back i.e. test

--- wp-content/themes/municipio-nsr/views/partials/header.blade.php
+++ wp-content/themes/municipio-nsr/views/partials/header.blade.php
@@ -1,11 +1,11 @@
 <header id="site-header" class="site-header">
-    <!-- <div class="print-only container">
+    <div class="print-only container">
         <div class="grid">
             <div class="grid-sm-12">
                 {!! municipio_get_logotype('standard') !!}
             </div>
         </div>
-    </div> -->
+    </div>
 
     @include('partials.header.' . $headerLayout['template'])
 </header>
