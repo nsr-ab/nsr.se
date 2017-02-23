89106342065c8d6c1eb02013a6abf3117ad05b7e
Johan Silvergrund <johan.silvergrund@hiq.se>
2017-02-23 13:07:22 +0100

    murphys law

--- wp-content/themes/municipio-nsr/views/partials/header.blade.php
+++ wp-content/themes/municipio-nsr/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header id="site-header" class="site-header">
-    <div class="print-doc container">
+    <div class="print-only container">
         <div class="grid">
             <div class="grid-sm-12">
                 {!! municipio_get_logotype('standard') !!}
