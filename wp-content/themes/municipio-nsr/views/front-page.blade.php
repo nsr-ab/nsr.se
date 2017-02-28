4249be6d6d67ace9a99a98f3f8323c87458d6f4d
Johan Silvergrund <johan.silvergrund@hiq.se>
2017-02-28 13:53:10 +0100

    readspeaker

--- wp-content/themes/municipio-nsr/views/front-page.blade.php
+++ wp-content/themes/municipio-nsr/views/front-page.blade.php
@@ -3,20 +3,23 @@
 @section('content')
 
     <div class="container main-container">
-        @if (function_exists('ReadSpeakerHelper_playButton'))
-            <div class="ReadspeakerPlayer">
-                <?php
 
-                ReadSpeakerHelper_player();
-                ?>
-            </div>
-        @endif
         @include('partials.breadcrumbs')
 
         <div class="grid {{ implode(' ', apply_filters('Municipio/Page/MainGrid/Classes', wp_get_post_parent_id(get_the_id()) != 0 ? array('no-margin-top') : array())) }}">
             @include('partials.sidebar-left')
 
             <div class="grid-md-12 grid-lg-12 grid-print-12" id="readspeaker-read">
+
+                @if (function_exists('ReadSpeakerHelper_playButton'))
+                    <div class="ReadspeakerPlayer">
+                        <?php
+
+                        ReadSpeakerHelper_player();
+                        ?>
+                    </div>
+                @endif
+
 
                 @if (is_active_sidebar('content-area-top'))
                     <div class="grid sidebar-content-area sidebar-content-area-top">
