d8cfecdbe98be72f5574a422539d9bc21c5413ab
tommymo <tommy.morberg@hiq.se>
2017-06-28 15:04:29 +0200

    Last fix for mobile search

--- wp-content/plugins/nsr-vc-extended/source/js/nsr-vc-extended.js
+++ wp-content/plugins/nsr-vc-extended/source/js/nsr-vc-extended.js
@@ -756,6 +756,7 @@ VcExtended.NSRExtend.Extended = (function ($) {
                                 if (spost.terms.inlamningsstallen[int][lint]['lat'] && spost.terms.inlamningsstallen[int][lint]['long'])
                                     CityItem[lint] = [spost.terms.inlamningsstallen[int][lint]['city'], spost.terms.inlamningsstallen[int][lint]['lat'], spost.terms.inlamningsstallen[int][lint]['long'], spost.terms.inlamningsstallen[int][lint]['city'], latlongID];
 
+                                searchID = latlongID;
                                 if(latlongID)
                                     latlongID = 'id="'+latlongID+'"';
 
