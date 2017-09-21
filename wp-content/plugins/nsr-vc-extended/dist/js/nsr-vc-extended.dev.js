b16b9683cbee2184425741a4093075503f047522
Johan Silvergrund <johan@silvergrund.se>
2017-09-21 15:55:54 +0200

    lint

--- wp-content/plugins/nsr-vc-extended/dist/js/nsr-vc-extended.dev.js
+++ wp-content/plugins/nsr-vc-extended/dist/js/nsr-vc-extended.dev.js
@@ -728,7 +728,7 @@ VcExtended.NSRExtend.Extended = (function ($) {
                             var locationmap;
                             var setNonLink = '';
                             for (var lint = 0; lint < spost.post_meta.inlamningsstallen[int].length; lint++) {
-                                 console.log(lint);
+                            
                                 if (spost.post_meta.inlamningsstallen[int][lint]['pageurl']) {
 
                                     inlLink = '';
@@ -771,11 +771,13 @@ VcExtended.NSRExtend.Extended = (function ($) {
 
                                 latlongID = '';
 
-                                if (lint > 5)
-                                    hideStuff = 'hide';
                                 if (spost.post_meta.inlamningsstallen[int][lint]['city'] != null) {
                                     if (!inlLink)
                                         setNonLink = 'nullLink';
+
+                                    if (lint > 5)
+                                        hideStuff = 'hide';
+
                                     sortHTML += '<li searchid="' + searchID + '" ' + latlongID + ' ' + latlong + ' class="' + setNonLink + ' ' + locationmap + ' ' + hideStuff + '" ' + inlineClick + '> ' + inlLink + spost.post_meta.inlamningsstallen[int][lint]['city'] + inLinkClose + '</li>';
                                     tabMobile_inl += '<li searchid="' + searchID + '" ' + latlongID + ' ' + latlong + ' class="' + setNonLink + ' ' + locationmap + ' ' + hideStuff + '" ' + inlineClick + '> ' + inlLink + spost.post_meta.inlamningsstallen[int][lint]['city'] + inLinkClose + '</li>';
                                 }
