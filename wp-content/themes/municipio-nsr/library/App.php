dba4ef2ea02aaccf020086d3659e8a1110a51e0a
Johan Silvergrund <johan@silvergrund.se>
2017-09-22 11:28:20 +0200

    headers

--- wp-content/themes/municipio-nsr/library/App.php
+++ wp-content/themes/municipio-nsr/library/App.php
@@ -54,9 +54,7 @@ class App
         $this->setBackgroundClass();
 
 
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-        header('Access-Control-Allow-Methods: GET, POST, PUT');
+        add_filter( 'allowed_http_origin', '__return_true' );
 
     }
 
