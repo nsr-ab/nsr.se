a83bc3d6e259374416d862e291e12573116b55ca
Johan Silvergrund <johan@silvergrund.se>
2017-09-27 10:40:07 +0200

    analytics

--- wp-content/plugins/nsr-vc-extended/source/js/nsr-vc-extended.js
+++ wp-content/plugins/nsr-vc-extended/source/js/nsr-vc-extended.js
@@ -39,6 +39,7 @@ VcExtended.NSRExtend.Extended = (function ($) {
         _gaq.push(['_setAccount', 'UA-92267061-1']); // your ID/profile
         _gaq.push(['_trackPageview']);
 
+        Extended.prototype.Analytics();
 
         /* Default search */
         Extended.prototype.DefaultSiteSearch();
@@ -110,6 +111,20 @@ VcExtended.NSRExtend.Extended = (function ($) {
 
 
     };
+
+    /**
+     *  Analytics
+     *  Tracking site search
+     */
+    Extended.prototype.Analytics = function () {
+
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+    }
 
     /**
      *  DefaultSiteSearch
@@ -473,7 +488,7 @@ VcExtended.NSRExtend.Extended = (function ($) {
             }
         }).done(function (result) {
 
-            _gaq.push(['_trackEvent', 'site-search', data.action, searchTerm, 'resultat:'+result.length]);
+            _gaq.push(['_trackEvent', 'site-search', data.action, searchTerm, 'resultat:' + result.length]);
 
 
             if (data.action === 'fetchDataFromElasticSearch') {
