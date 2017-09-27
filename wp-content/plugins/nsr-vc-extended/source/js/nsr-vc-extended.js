e5ef8fed65a5cafd0501957af7a635faec972c48
Johan Silvergrund <johan@silvergrund.se>
2017-09-27 11:19:48 +0200

    analytics

--- wp-content/plugins/nsr-vc-extended/source/js/nsr-vc-extended.js
+++ wp-content/plugins/nsr-vc-extended/source/js/nsr-vc-extended.js
@@ -38,8 +38,7 @@ VcExtended.NSRExtend.Extended = (function ($) {
 
         _gaq.push(['_setAccount', 'UA-92267061-1']); // your ID/profile
         _gaq.push(['_trackPageview']);
-
-        Extended.prototype.Analytics();
+        
 
         /* Default search */
         Extended.prototype.DefaultSiteSearch();
@@ -111,21 +110,7 @@ VcExtended.NSRExtend.Extended = (function ($) {
 
 
     };
-
-    /**
-     *  Analytics
-     *  Tracking site search
-     */
-    Extended.prototype.Analytics = function () {
-
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    }
-
+    
     /**
      *  DefaultSiteSearch
      *  Search via searchModal window
@@ -489,6 +474,7 @@ VcExtended.NSRExtend.Extended = (function ($) {
         }).done(function (result) {
 
             _gaq.push(['_trackEvent', 'site-search', data.action, searchTerm, 'resultat:' + result.length]);
+            parent.ga('send', 'event', 'site-search-devs', searchTerm);
 
 
             if (data.action === 'fetchDataFromElasticSearch') {
--- wp-content/themes/municipio-nsr/views/partials/footer/footer-compressed.blade.php
+++ wp-content/themes/municipio-nsr/views/partials/footer/footer-compressed.blade.php
@@ -63,3 +63,9 @@
         </div>
     </div>
 </footer>
+<script type="text/javascript">  (function() {
+        var ga = document.createElement('script');     ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:'   == document.location.protocol ? 'https://ssl'   : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+</script>
