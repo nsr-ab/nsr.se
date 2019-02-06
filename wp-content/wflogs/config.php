6817c0faaf0a982ac0e0cf70800b67c7f68a51be
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2019-02-06 21:40:35 +0100

    r

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1543430190;s:7:"authKey";s:64:"T>,hH0Ndx7w37KbUhh.f?:?]-l^pKQ;f{}gJ>!Z|bZfMb<9,I#5j6R5 VVwz%1*9";s:7:"version";s:5:"1.0.4";}
